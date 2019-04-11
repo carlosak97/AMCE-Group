84d2edad0453d5919b3958bd4ee7035ab279c151
Elie <eliemayo@yahoo.com>
2019-04-12 00:36:57 +0300

    Improved the Code

--- POS_proj_git/POS_proj_git/Lookup.Designer.vb
+++ POS_proj_git/POS_proj_git/Lookup.Designer.vb
@@ -22,32 +22,24 @@ Partial Class Lookup
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DGVData = New System.Windows.Forms.DataGridView()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.EmployeesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ItemsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.CustomersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.BackToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        CType(Me.DGVData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DGVData = New System.Windows.Forms.DataGridView()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.DGVData, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'DGVData
-        '
-        Me.DGVData.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.DGVData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGVData.Location = New System.Drawing.Point(-1, 71)
-        Me.DGVData.Name = "DGVData"
-        Me.DGVData.Size = New System.Drawing.Size(806, 364)
-        Me.DGVData.TabIndex = 58
         '
         'MenuStrip1
         '
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EmployeesToolStripMenuItem, Me.ItemsToolStripMenuItem, Me.CustomersToolStripMenuItem, Me.BackToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(801, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(676, 24)
         Me.MenuStrip1.TabIndex = 59
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -75,27 +67,48 @@ Partial Class Lookup
         Me.BackToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
         Me.BackToolStripMenuItem.Text = "Back"
         '
+        'DGVData
+        '
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.Silver
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent
+        Me.DGVData.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.DGVData.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.Gray
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Info
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Transparent
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DGVData.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.DGVData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGVData.Location = New System.Drawing.Point(0, 50)
+        Me.DGVData.Name = "DGVData"
+        Me.DGVData.Size = New System.Drawing.Size(676, 234)
+        Me.DGVData.TabIndex = 60
+        '
         'Lookup
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(801, 433)
+        Me.ClientSize = New System.Drawing.Size(676, 282)
         Me.Controls.Add(Me.DGVData)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Lookup"
         Me.Text = "Lookup"
-        CType(Me.DGVData, System.ComponentModel.ISupportInitialize).EndInit()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.DGVData, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents DGVData As System.Windows.Forms.DataGridView
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
     Friend WithEvents EmployeesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents ItemsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents CustomersToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents BackToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DGVData As System.Windows.Forms.DataGridView
 End Class
